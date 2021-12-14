26e94f30f6a3560284a1ce4b6c9c0b5162b250c0
Jake Motta <motta_jake@Yahoo.com>
2021-12-14 05:07:12 -0800

    rollup

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-export default function Button({ label, backgroundColor, onClick }) {
+export default function Button() {
     return (
-        <button onClick={onClick}
-            style={backgroundColor && { backgroundColor }}>
-            {label}
-        </button>
+        <div>This is a button</div>
     );
 }
-
-Button.propTypes = {
-    backgroundColor: PropTypes.string,
-    label: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-    backgroundColor: null,
-    onClick: undefined,
-};
